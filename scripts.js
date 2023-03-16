1eaeeb006bf6ccff7f8740a91775a2aaa0c0df81
Ömer Islamoglu <trinsyca@gmail.com>
2023-03-16 08:08:41 +0300

    bistbilisim.com

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('nav');
+const translate = document.querySelector('.translate');
+const bar = document.querySelector('.VIpgJd-ZVi9od-ORHb');
+window.addEventListener('scroll' , () =>
+{
+   if(window.pageYOffset > 750)
+   {
+      translate.style.background = 'rgba(0, 0, 0, 0.3)';
+      translate.style.boxShadow = '0 0 5px 2px #000';
+   }
+   else
+   {
+      translate.style.background = 'transparent';
+      translate.style.boxShadow = 'none';
+   }
+   nav.classList.toggle('nav-anim',window.scrollY > 1000);
+});
