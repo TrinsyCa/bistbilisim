4e44ec929fa3a9925a9a1a7a243826c289fa4019
Ömer Islamoglu <trinsyca@gmail.com>
2023-04-04 08:46:45 +0300

    bistbilisim.com

--- scripts.js
+++ scripts.js
@@ -1,17 +1,68 @@
 const nav = document.querySelector('nav');
 const translate = document.querySelector('.translate');
-const bar = document.querySelector('.VIpgJd-ZVi9od-ORHb');
+const headertxt = document.querySelector('.header-txt');
+const studentcol = document.querySelector('.students-col');
+const stBtn = document.querySelector('.students-detail .btn a');
+const fieldsRow = document.querySelector('.fields .fields-row');
+const stTitle = document.querySelector('.students-detail .title h2');
+const fieldsTitle = document.querySelector('.fields h2');
+const galleryTitle = document.querySelector('.gallery h2');
+
 window.addEventListener('scroll' , () =>
 {
+   if(window.pageYOffset > 400 && window.pageYOffset < 1500)
+   {
+      fieldsTitle.style.opacity = '1';
+      fieldsRow.style.scale = '0.97';
+      fieldsRow.style.opacity = '1';
+   }
+   else
+   {
+      fieldsTitle.style.opacity = '0';
+      fieldsRow.style.scale = '0.4';
+      fieldsRow.style.opacity = '0';
+   }
    if(window.pageYOffset > 900)
    {
       translate.style.background = 'rgba(0, 0, 0, 0.3)';
       translate.style.boxShadow = '0 0 5px 2px #000';
    }
-   else
+   else if(window.pageYOffset < 900)
    {
       translate.style.background = 'transparent';
       translate.style.boxShadow = 'none';
+   }
+   if(window.pageYOffset > 800)
+   {
+      headertxt.style.paddingTop = '80px';
+   }
+   if(window.pageYOffset < 800)
+   {
+      headertxt.style.paddingTop = '0';
+   }
+   if(window.pageYOffset > 1500 && window.pageYOffset < 2500)
+   {
+      studentcol.style.opacity = '1';
+      studentcol.style.scale = '1.0';
+      stBtn.style.opacity = '1';
+      stBtn.style.scale = '1.0';
+   }
+   else
+   {
+      studentcol.style.opacity = '0';
+      studentcol.style.scale = '0.4';
+      stBtn.style.opacity = '0';
+      stBtn.style.scale = '0.1';
+   }
+   if(window.pageYOffset > 1200)
+   {
+      stTitle.style.opacity = '1';
+      stTitle.style.scale = '1';
+   }
+   else
+   {
+      stTitle.style.opacity = '0';
+      stTitle.style.scale = '0';
    }
    nav.classList.toggle('nav-anim',window.scrollY > 900);
 });
